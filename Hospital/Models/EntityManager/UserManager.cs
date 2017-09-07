dc13e7790fe9159076a9d10ecd31cf727b56c6dc
AtaMark <markjohnkeith2013@gmail.com>
2017-09-07 17:27:57 +0300

    Added User Entity Model

---
+++ Hospital/Models/EntityManager/UserManager.cs
@@ -0,0 +1,20 @@
+﻿using Hospital.Models.DataBase;
+using Hospital.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital.Models.EntityManager
+{
+    public class UserManager
+    {
+        public void AddUser(UserModel user) {
+            using (HOSPITALEntities db = new HOSPITALEntities())
+            {
+               
+            }
+
+        }
+    }
+}
---
+++ Hospital/Models/ViewModel/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hospital.Models.ViewModel
+{
+    public class UserModel
+    {
+        [Key]
+        public int idUser { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string name { get; set; }
+        [Required]
+        public int departmentId { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        [Required]
+        [Display(Name = "Contact")]
+        public string user_contact { get; set; }
+    }
+}
