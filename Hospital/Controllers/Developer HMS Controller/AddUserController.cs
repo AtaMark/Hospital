17e76ae05b1db4dfe92d866b2a2c5944915a05f9
AtaMark <markjohnkeith2013@gmail.com>
2017-09-13 15:03:34 +0300

    Added AddUserController

---
+++ Hospital/Controllers/Developer HMS Controller/AddUserController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hospital.Controllers.Developer_HMS_Controller
+{
+    public class AddUserController : Controller
+    {
+        // GET: AddUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AddUser/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AddUser/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AddUser/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AddUser/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AddUser/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AddUser/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AddUser/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Hospital/Models/EntityManager/UserManager.cs
+++ Hospital/Models/EntityManager/UserManager.cs
@@ -7,12 +7,18 @@ using System.Web;
 
 namespace Hospital.Models.EntityManager
 {
+    //This class will handle CRUD operations for User Entity
     public class UserManager
     {
         public void AddUser(UserModel user) {
             using (HOSPITALEntities db = new HOSPITALEntities())
             {
-               
+                User newUser = new User();
+                newUser.name = user.name;
+                newUser.email = user.email;
+                newUser.departmentID = user.departmentId;
+                newUser.user_contact = user.user_contact;
+                db.Users.Add(newUser);
             }
 
         }
